cda81ed9cc5ad586643782c085285d11c697e371
Ma-Vin <ma.vin.github@gmail.com>
2024-03-09 14:40:25 +0100

    feat: add version github json model

---
+++ service/github_model/version.go
@@ -0,0 +1,41 @@
+package github_model
+
+type JsonPackageType string
+
+const (
+	NPM       JsonPackageType = "npm"
+	MAVEN     JsonPackageType = "maven"
+	RUBYGEMS  JsonPackageType = "rubygems"
+	DOCKER    JsonPackageType = "docker"
+	NUGET     JsonPackageType = "nuget"
+	CONTAINER JsonPackageType = "container"
+)
+
+// package version definition for a user, see also: https://docs.github.com/en/rest/packages/packages?apiVersion=2022-11-28#get-a-package-version-for-a-user
+type Version struct {
+	Id             int      `json:"id"`
+	Name           string   `json:"name"`
+	Url            string   `json:"url"`
+	PackageHtmlUrl string   `json:"package_html_url"`
+	HtmlUrl        string   `json:"html_url"`
+	License        string   `json:"license"`
+	Description    string   `json:"description"`
+	CreatedAt      string   `json:"created_at"`
+	UpdatedAt      string   `json:"updated_at"`
+	DeletedAt      string   `json:"deleted_at"`
+	Metadata       Metadata `json:"metadata"`
+}
+
+type Metadata struct {
+	PackageType JsonPackageType `json:"package_type"`
+	Container   Container       `json:"container"`
+	Docker      Docker          `json:"docker"`
+}
+
+type Container struct {
+	Tags []string `json:"tags"`
+}
+
+type Docker struct {
+	Tags []string `json:"tag"`
+}
