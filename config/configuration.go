9ddf1e558092e0df53eca271fa90d080ee50f1b4
Ma-Vin <ma.vin.github@gmail.com>
2024-03-10 19:46:22 +0100

    feat: add config variants for deletion

--- config/configuration.go
+++ config/configuration.go
@@ -3,6 +3,7 @@ package config
 import (
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -23,8 +24,16 @@ type Config struct {
 	PackageType string
 	// name of the package (not nil)
 	PackageName string
+	// name of a version to delete
+	VersionNameToDelete string
 	// indicator whether to delete snapshots or not
 	DeleteSnapshots bool
+	// Number major versions to keep
+	NumberOfMajorVersionsToKeep int
+	// Number minor versions to keep
+	NumberOfMinorVersionsToKeep int
+	// Number patch versions to keep
+	NumberOfPatchVersionsToKeep int
 	// token which is to use to authenticate against github rest api (not nil)
 	GithubToken string
 }
@@ -44,7 +53,11 @@ func ReadConfiguration() *Config {
 	config.User = getTrimEnv("USER")
 	config.PackageType = mapToPackageType(getTrimEnv("PACKAGE_TYPE"))
 	config.PackageName = getTrimEnv("PACKAGE_NAME")
-	config.DeleteSnapshots = strings.EqualFold("true", getTrimEnv("DELETE_SNAPSHOTS"))
+	config.VersionNameToDelete = getTrimEnv("VERSION_NAME_TO_DELETE")
+	config.DeleteSnapshots = getBoolEnv("DELETE_SNAPSHOTS")
+	config.NumberOfMajorVersionsToKeep = getIntEnv("NUMBER_MAJOR_TO_KEEP")
+	config.NumberOfMinorVersionsToKeep = getIntEnv("NUMBER_MINOR_TO_KEEP")
+	config.NumberOfPatchVersionsToKeep = getIntEnv("NUMBER_PATCH_TO_KEEP")
 	config.GithubToken = getTrimEnv("GITHUB_TOKEN")
 
 	printConfig(&config)
@@ -59,6 +72,29 @@ func ReadConfiguration() *Config {
 // determines an environment variable and return it as trimmed string
 func getTrimEnv(envName string) string {
 	return strings.TrimSpace(os.Getenv(envName))
+}
+
+// determines an environment variable and return it as bool
+func getBoolEnv(envName string) bool {
+	return strings.EqualFold("true", getTrimEnv(envName))
+}
+
+// determines an environment variable and return it as int.
+func getIntEnv(envName string) int {
+	envValue := getTrimEnv(envName)
+	if envValue != "" {
+		res, err := strconv.Atoi(os.Getenv(envName))
+		if err != nil {
+			log.Println(err)
+			return -1
+		}
+		if res <= 0 {
+			log.Println("Only positiv values are allowed but found ", res, " for environment variable ", envName)
+			return -1
+		}
+		return res
+	}
+	return -1
 }
 
 // maps a given string to a package type
@@ -89,20 +125,36 @@ func isValid(config *Config) bool {
 		log.Println("Missing GitHub token")
 		return false
 	}
+	if config.VersionNameToDelete == "" && !config.DeleteSnapshots &&
+		config.NumberOfMajorVersionsToKeep <= 0 && config.NumberOfMinorVersionsToKeep <= 0 && config.NumberOfPatchVersionsToKeep <= 0 {
+		log.Println("Nothing configured to delete: set a conrete version name, snapshot deletion or major, minor or patch to keep")
+		return false
+	}
 	return true
 }
 
 // prints a given configuration to the standard output
 func printConfig(config *Config) {
 	log.Println("Read configuration", config.Organization)
-	log.Println("  Organization:    ", config.Organization)
-	log.Println("  User:            ", config.User)
-	log.Println("  PackageType:     ", config.PackageType)
-	log.Println("  PackageName:     ", config.PackageName)
-	log.Println("  DeleteSnapshots: ", config.DeleteSnapshots)
+	log.Println("  Organization:        ", config.Organization)
+	log.Println("  User:                ", config.User)
+	log.Println("  PackageType:         ", config.PackageType)
+	log.Println("  PackageName:         ", config.PackageName)
+	log.Println("  VersionNameToDelete: ", config.VersionNameToDelete)
+	log.Println("  DeleteSnapshots:     ", config.DeleteSnapshots)
+	printPositiv("  MajorVersionsToKeep: ", config.NumberOfMajorVersionsToKeep)
+	printPositiv("  MinorVersionsToKeep: ", config.NumberOfMinorVersionsToKeep)
+	printPositiv("  PatchVersionsToKeep: ", config.NumberOfPatchVersionsToKeep)
 	if config.GithubToken != "" {
-		log.Println("  GithubToken:      ***")
+		log.Println("  GithubToken:          ***")
 	} else {
 		log.Println("  GithubToken:")
 	}
+}
+
+func printPositiv(text string, value int) {
+	if 0 < value {
+		log.Println(text, value)
+	}
+	log.Println(text)
 }
