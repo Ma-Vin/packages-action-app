41c3970693fd1c72e2a2b8cf1f618bd006096145
Ma-Vin <ma.vin.github@gmail.com>
2024-03-17 20:05:16 +0100

    feat: execute deletion at main process

--- action.go
+++ action.go
@@ -4,20 +4,24 @@ import (
 	"log"
 
 	"github.com/ma-vin/packages-action/config"
+	"github.com/ma-vin/packages-action/service"
 )
 
 // Main funtion to execute the actions process
 func main() {
 	log.Println("Start packages action")
 
-	_, err := config.ReadConfiguration()
+	var loadedConfig, err = config.ReadConfiguration()
 
-	checkkError(err)
+	checkError(err)
+
+	err = service.DeleteVersions(loadedConfig)
+	checkError(err)
 
 	log.Println("Packages action done")
 }
 
-func checkkError(err error) {
+func checkError(err error) {
 	if err != nil {
 		log.Fatalf("Packages action failed: %s", err)
 	}
